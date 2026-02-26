d1d92f60b43b99ebb8a1e7c6231fbd93a7af5547
muhammetguducu <84397069+MuhammetGuducu@users.noreply.github.com>
2026-02-26 02:10:36 +0100

    Add new URL paths for stock and API endpoints
    Some Endpoints are not allowed in this code version, as I didn't upload confidental files.

--- src/app/website/urls.py
+++ src/app/website/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
+    # Web Interface
     path('', views.home, name='home'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
@@ -10,5 +10,20 @@ urlpatterns = [
     path('update_device/<int:pk>', views.update_device, name='update_device'),
     path('analytics/', views.analytics, name='analytics'),
     path('backup_restore/', views.backup_restore, name='backup_restore'),
-    path('stock_history/', views.stock_history, name='stock_history')
+    path('stock_history/', views.stock_history, name='stock_history'),
+    path('stock_history/export/', views.export_stock_history,
+         name='export_stock_history'),
+    path('export_inventory/', views.export_current_stock,
+         name='export_current_stock'),
+    path('flash_device/', views.flash_device, name='flash_device'),
+    path('firmware/main.py', views.get_firmware_file, name='get_firmware'),
+    path('firmware/clone_files/', views.get_clone_files, name='get_clone_files'),
+
+    # API Endpoints for BLE Gateway
+    path('api/devices/register', views.api_register_device, name='api_register'),
+    path('api/inventory/update', views.api_update_inventory,
+         name='api_update_inventory'),
+    path('api/devices/<str:mac_address>/updates',
+         views.api_check_updates, name='api_check_updates'),
+    path('api/last_update', views.api_last_update_timestamp, name='api_last_update'),
 ]
